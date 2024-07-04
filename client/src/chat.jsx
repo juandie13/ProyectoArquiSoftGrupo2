15c2046cf3930df4a1ad10f2088b3f0b0af3c915
Juan Diego Castillo Carnero <juandiegomaster13@hotmail.com>
2024-07-03 23:50:19 -0500

    Seleccion de usuarios

--- client/src/chat.jsx
+++ client/src/chat.jsx
@@ -1,9 +1,13 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import Avatar from "./Avatar";
+import Logo from "./Logo";
+import { UserContext } from "./UserContext.jsx";
 
 export default function Chat() {
   const [ws, setWs] = useState(null);
   const [onlinePeople, setOnlinePeople] = useState({});
+  const [selectedUserId, setSelectedUserId] = useState(null);
+  const { username, id } = useContext(UserContext);
   useEffect(() => {
     const ws = new WebSocket("ws://localhost:4040");
     setWs(ws);
@@ -25,35 +29,40 @@ export default function Chat() {
     }
   }
 
+  const onlinePeopleExclOurUser = { ...onlinePeople };
+  delete onlinePeopleExclOurUser[id];
+
   return (
     <div className="flex h-screen">
-      <div className="bg-white w-1/3 pl-4 pt-4">
-        <div className="text-blue-600 font-bold flex gap-2 mb-4">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-6"
+      <div className="bg-white w-1/3">
+        <Logo />
+        {Object.keys(onlinePeopleExclOurUser).map((userId) => (
+          <div
+            key={userId}
+            onClick={() => setSelectedUserId(userId)}
+            className={
+              "border-b border-gray-100 flex items-center gap-2 cursor-pointer " +
+              (userId === selectedUserId ? "bg-blue-50" : "")
+            }
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5"
-            />
-          </svg>
-          ClassTalk
-        </div>
-        {Object.keys(onlinePeople).map((userId) => (
-          <div className="border-b border-gray-100 py-2 flex items-center gap-2 cursor-pointer">
-            <Avatar username={onlinePeople[userId]} userId={userId}/>
-            <span className="text-grey-800">{onlinePeople[userId]}</span>
+            {userId === selectedUserId && (
+              <div className="w-1 bg-blue-500 h-12 rounded-r-md"></div>
+            )}
+            <div className="flex gap-2 py-2 pl-4 items-center">
+              <Avatar username={onlinePeople[userId]} userId={userId} />
+              <span className="text-grey-800">{onlinePeople[userId]}</span>
+            </div>
           </div>
         ))}
       </div>
       <div className="flex flex-col bg-blue-300 w-2/3 p-2">
-        <div className="flex-grow">Mensajes con personas seleccionadas</div>
+        <div className="flex-grow">
+          {!selectedUserId && (
+            <div className="flex h-full flex-grow items-center justify-center">
+              <div className="text-gray-700">&larr; Elija a un usuario de la izquierda</div>
+            </div>
+          )}
+        </div>
         <div className="flex gap-2 mx-2">
           <input
             type="text"
